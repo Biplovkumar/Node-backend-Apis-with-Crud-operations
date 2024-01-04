817a23dcae1448d3ea0fa69d24bc7dbfec757511
Biplov Kumar <Kumarb@thepaycocld.onmicrosoft.com>
2024-01-05 01:16:50 +0530

    Working till update product with token

--- app.js
+++ app.js
@@ -2,48 +2,67 @@
 const express = require("express");
 require("./db/config");
 const User = require('./db/User');
+const Product = require("./db/Product")
+
+const Jwt = require('jsonwebtoken');
+const jwtKey = 'e-com';
+
 const app = express();
 const cors = require("cors");
-const Product = require("./db/Product")
 const port = 4000
 
 app.use(express.json());
 app.use(cors());
 
 app.post("/register", async (req, resp) => {
-  let user = new User(req.body);
-  let result = await user.save();
-  result = result.toObject();
-  delete result.password
-  resp.send(result)
+ if (!req.body.name || !req.body.email ||  !req.body.password) {
+    return resp.status(400).send({ result: "All fields are required"});
+  }else{
+    let user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password
+    Jwt.sign({result}, jwtKey, {expiresIn:"2h"},(err,token)=>{
+        if(err){
+            resp.status(401).send({ result:"Something went wrong"}); 
+        }
+        resp.send({result,auth:token})
+    })
+   }
 })
 
 app.post("/login", async (req, resp) => {
-  if (req.body.password && req.body.email) {
-      let user = await User.findOne(req.body).select("-password");
-      if (user) {
-        resp.send({user,auth:'token'})
-          // Jwt.sign({user}, jwtKey, {expiresIn:"2h"},(err,token)=>{
-          //     if(err){
-          //         resp.send("Something went wrong")  
-          //     }
-          //     resp.send({user,auth:token})
-          // })
-      } else {
-          resp.send({ result: "No User found" })
-      }
-  } else {
-      resp.send({ result: "No User found" })
-  }
+  if (!req.body.email ||  !req.body.password) {
+       resp.status(400).send({ result:"Enter correct details"});
+  }else{
+        let user = await User.findOne(req.body).select("-password");
+
+        if (user) {
+            Jwt.sign({user}, jwtKey, {expiresIn:"2h"},(err,token)=>{
+                if(err){
+                    resp.status(401).send({ result:"Something went wrong"});
+                }
+                resp.send({user,auth:token})
+            })
+        } else {
+            resp.status(400).send({ result:"No User found"});
+        }
+    }
 });
 
-app.post("/add-product", async (req, resp) => {
+//Add product
+app.post("/add-product", verityToken, async (req, resp) => {
+if (!req.body.name ||  !req.body.price || !req.body.category ||  !req.body.company) {
+    resp.status(400).send({ result:"Enter correct details"});
+  }else{
   let product = new Product(req.body);
   let result = await product.save();
   resp.send(result);
+}
 });
 
-app.get("/products", async (req, resp) => {
+//Get all products
+app.get("/products", verityToken, async (req, resp) => {
   const products = await Product.find();
   if (products.length > 0) {
       resp.send(products)
@@ -52,13 +71,14 @@ app.get("/products", async (req, resp) => {
   }
 });
 
-app.delete("/product/:id", async (req, resp) => {
+//Delete one product
+app.delete("/product/:id", verityToken, async (req, resp) => {
   let result = await Product.deleteOne({ _id: req.params.id });
   resp.send(result)
 });
 
-
-app.get("/product/:id", async (req, resp) => {
+//get perticular product detail
+app.get("/product/:id", verityToken, async (req, resp) => {
   let result = await Product.findOne({ _id: req.params.id })
   if (result) {
       resp.send(result)
@@ -67,7 +87,8 @@ app.get("/product/:id", async (req, resp) => {
   }
 })
 
-app.put("/product/:id", async (req, resp) => {
+//Update perticular product
+app.put("/product/:id", verityToken, async (req, resp) => {
 let result = await Product.updateOne(
   { _id: req.params.id },
   { $set: req.body }
@@ -75,36 +96,8 @@ let result = await Product.updateOne(
 resp.send(result)
 });
 
-
-app.get("/search/:key", async (req, resp) => {
-  let result = await Product.find({
-      "$or": [
-          {
-              name: { $regex: req.params.key }  
-          },
-          {
-              company: { $regex: req.params.key }
-          },
-          {
-              category: { $regex: req.params.key }
-          }
-      ]
-  });
-  resp.send(result);
-})
-
-app.post("/test", (req, resp) => {
-  if (!req.body || Object.keys(req.body).length === 0) {
-    return resp.status(400).send({ result: "No data found" });
-  }else{
-// Generate a random token by 6 digit values
-const randomNumber = Math.floor(Math.random() * 900000) + 100000;
-const concatenatedObject = { ...req.body, ...{value:randomNumber} };
-resp.send(concatenatedObject);
-  }
-});
-
-app.get("/search/:key", async (req, resp) => {
+//Search product with key
+app.get("/search/:key", verityToken, async (req, resp) => {
     let result = await Product.find({
         "$or": [
             {
@@ -121,12 +114,51 @@ app.get("/search/:key", async (req, resp) => {
     resp.send(result);
 })
 
+//Verify token
+function verityToken(req, resp, next){
+let token = req.headers['authorization'];
+if(token){
+token = token.split(' ')[1];
+Jwt.verify(token, jwtKey, (err, valid)=>{
+    if(err){
+    resp.status(401).send({result: 'Please provide a valid token'})
+    }else{
+    next();
+    }
+})}
+ else{
+      resp.status(403).send({result: 'Please provide a token'})
+     }
+}
+
+//Verify all filled
+function areAllFieldsFilled(obj) {
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (obj[key] == null || obj[key] === undefined || obj[key] === '') {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+//Testing api
+app.post("/test", (req, resp) => {
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return resp.status(400).send({ result: "No data found" });
+  }else{
+// Generate a random token by 6 digit values
+const randomNumber = Math.floor(Math.random() * 900000) + 100000;
+const concatenatedObject = { ...req.body, ...{value:randomNumber} };
+resp.send(concatenatedObject);
+  }
+});
 
 //This is for testing purpose
-     app.get('/', (req, res) => {
+  app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 
 //This is for Terminal
 app.listen(port, () => {
